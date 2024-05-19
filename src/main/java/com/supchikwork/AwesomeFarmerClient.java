a736934a308d933c209bfb8c810e783b2562c4b5
loppisspy <125576000+loppisspy@users.noreply.github.com>
2024-05-20 02:31:34 +0300

    added farmer mode and fix
    There is now a farmer mode where you can only break crops. Also fixed kelp issue, when on servers they were not stacked in kelp_plant, because of which it was impossible to break them in the previous version.

--- src/main/java/com/supchikwork/AwesomeFarmerClient.java
+++ src/main/java/com/supchikwork/AwesomeFarmerClient.java
@@ -1,54 +1,98 @@
 package com.supchikwork;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.CropBlock;
-import net.minecraft.block.BeetrootsBlock;
-import net.minecraft.block.NetherWartBlock;
-import net.minecraft.block.SweetBerryBushBlock;
-import net.minecraft.block.CocoaBlock;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.BeetrootsBlock;
+import net.minecraft.block.CropBlock;
+import net.minecraft.block.CocoaBlock;
+import net.minecraft.block.NetherWartBlock;
+import net.minecraft.block.SweetBerryBushBlock;
+import net.minecraft.block.TorchflowerBlock;
+import net.minecraft.block.PitcherCropBlock;
+import org.lwjgl.glfw.GLFW;
 
+import java.util.List;
 import java.util.Map;
 import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 
 public class AwesomeFarmerClient implements ClientModInitializer {
+    private boolean isFarmerModeEnabled = false;
+    private final KeyBinding toggleFarmerMode = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "key.awesomefarmer.farmerMode",
+            InputUtil.Type.KEYSYM,
+            GLFW.GLFW_KEY_UNKNOWN,
+            "category.awesomefarmer"
+    ));
 
-    private final Map<Block, Predicate<BlockState>> PLANTS = Map.of(
-            Blocks.WHEAT, state -> state.get(CropBlock.AGE) != 7,
-            Blocks.CARROTS, state -> state.get(CropBlock.AGE) != 7,
-            Blocks.POTATOES, state -> state.get(CropBlock.AGE) != 7,
+    private final Map<Block, Predicate<BlockState>> CROPS = Map.of(
             Blocks.BEETROOTS, state -> state.get(BeetrootsBlock.AGE) != 3,
+            Blocks.CARROTS, state -> state.get(CropBlock.AGE) != 7,
+            Blocks.COCOA, state -> state.get(CocoaBlock.AGE) != 2,
             Blocks.NETHER_WART, state -> state.get(NetherWartBlock.AGE) != 3,
+            Blocks.POTATOES, state -> state.get(CropBlock.AGE) != 7,
             Blocks.SWEET_BERRY_BUSH, state -> state.get(SweetBerryBushBlock.AGE) != 3,
-            Blocks.COCOA, state -> state.get(CocoaBlock.AGE) != 2
+            Blocks.WHEAT, state -> state.get(CropBlock.AGE) != 7,
+            Blocks.TORCHFLOWER, state -> state.get(TorchflowerBlock.AGE) != 2,
+            Blocks.PITCHER_CROP, state -> state.get(PitcherCropBlock.AGE) != 4
     );
 
     private final Map<Block, BiPredicate<BlockPos, World>> TALL_PLANTS = Map.of(
-            Blocks.SUGAR_CANE, (pos, world) -> world.getBlockState(pos.down(1)).getBlock() != world.getBlockState(pos).getBlock(),
             Blocks.BAMBOO, (pos, world) -> world.getBlockState(pos.down(1)).getBlock() != world.getBlockState(pos).getBlock(),
             Blocks.CACTUS, (pos, world) -> world.getBlockState(pos.down(1)).getBlock() != world.getBlockState(pos).getBlock(),
+            Blocks.KELP, (pos, world) -> {
+                    if (world.getBlockState(pos.down(1)).getBlock() == world.getBlockState(pos).getBlock()) {
+                        return false;
+                    }
+
+                    return world.getBlockState(pos.down(1)).getBlock() != Blocks.KELP_PLANT;
+                },
             Blocks.KELP_PLANT, (pos, world) -> world.getBlockState(pos.down(1)).getBlock() != world.getBlockState(pos).getBlock(),
-            Blocks.KELP, (pos, world) -> world.getBlockState(pos.down(1)).getBlock() != Blocks.KELP_PLANT
+            Blocks.SUGAR_CANE, (pos, world) -> world.getBlockState(pos.down(1)).getBlock() != world.getBlockState(pos).getBlock()
+    );
+
+    private final List<Block> CUCURBITS = List.of(
+            Blocks.PUMPKIN,
+            Blocks.MELON
     );
 
     @Override
     public void onInitializeClient() {
-        AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) -> {
-            final BlockState state = world.getBlockState(pos);
-            final Block block = state.getBlock();
-
-            if (PLANTS.containsKey(block) && PLANTS.get(block).test(state) && !player.isSneaking()) {
-                return ActionResult.FAIL;
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (this.toggleFarmerMode.wasPressed() && client.currentScreen == null) {
+                this.isFarmerModeEnabled = !this.isFarmerModeEnabled;
             }
+        });
 
-            if (TALL_PLANTS.containsKey(block) && TALL_PLANTS.get(block).test(pos, world) && !player.isSneaking()) {
+        AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) -> {
+            if (this.isFarmerModeEnabled) {
+                final BlockState state = world.getBlockState(pos);
+                final Block block = state.getBlock();
+
+                if (CROPS.containsKey(block) || TALL_PLANTS.containsKey(block)) {
+                    Predicate<BlockState> cropPredicate = CROPS.get(block);
+                    BiPredicate<BlockPos, World> tallPlantPredicate = TALL_PLANTS.get(block);
+
+                    if ((cropPredicate != null && cropPredicate.test(state)) || tallPlantPredicate != null && tallPlantPredicate.test(pos, world)) {
+                        return ActionResult.FAIL;
+                    } else {
+                        return ActionResult.PASS;
+                    }
+                } else if (CUCURBITS.contains(block)) {
+                    return ActionResult.PASS;
+                }
+
                 return ActionResult.FAIL;
             }
 
